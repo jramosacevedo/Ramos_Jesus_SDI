cdb5fc64cab2074f63302c7801df8730239260e6
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-07 00:07:45 -0400

    Fifth Commit

--- Ramos_Jesus_Output_Assignment/js/script.js
+++ Ramos_Jesus_Output_Assignment/js/script.js
@@ -1,6 +1,10 @@
 /**
  * Created by Jesus on 8/6/15.
  */
+
+    // Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Output Assignment
+
+
 var livesInpuertorico = true; //Boolean Variable  States that Jesus Lives in Puerto Rico
 
 var howManykidsjesushas = 3; //Number Variable  Shows how Many Kids Jesus has
