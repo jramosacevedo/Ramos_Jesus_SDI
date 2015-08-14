4b5a6aa04ed16a8a8cfd777ffa6fdb2bbc23a80c
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-13 23:00:20 -0400

    Prompts added

--- Ramos_Jesus_Expressions_Assignment./js/script.js
+++ Ramos_Jesus_Expressions_Assignment./js/script.js
@@ -1,8 +1,16 @@
 
 
-//Variables 
-var myResidenceTown;
+//Variables
+var myResidenceCity;
 var MyResidenceState;
 var yearsLivingInState;
 var usCitizen = true;
 
+// prompts
+
+myResidenceCity = prompt("Enter your City");
+myResidenceState = prompt("Enter your State");
+yearsLivingInstate = prompt(" How long have you lived there?");
+
+//outputs
+console.log("You live in " + myResidenceCity + myResidenceState);
