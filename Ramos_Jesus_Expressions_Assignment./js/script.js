9740e2339e954a3110d4ad71573ab89f43bda1ba
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-13 23:39:38 -0400

    new prompts

--- Ramos_Jesus_Expressions_Assignment./js/script.js
+++ Ramos_Jesus_Expressions_Assignment./js/script.js
@@ -10,11 +10,11 @@ var usCitizen = true;
 
 myResidenceCity = prompt("Enter your City");
 myResidenceState = prompt("Enter your State");
-//yearsLivingInstate = prompt(" How many years have you lived there?");
+yearsLivingInstate = prompt("Years living there");
 //usCitizen = prompt("How many years have you been a US Citizen?");
-
+yearsLivingInState = Number(yearsLivingInState);
 //outputs
 console.log("You live in " + myResidenceCity + " " + myResidenceState);
 console.log("it is" + " " + usCitizen +" " + "that you are a US Citizen");
-//console.log("You have been living in Puerto Rico for" + yearsLivingInState + 5 + "years more than me");
-
+//console.log("You have been living in Puerto Rico for" + yearsLivingInState + "years more than me");
+console.log("You have been living there for " + yearsLivingInState + " years more than me");
