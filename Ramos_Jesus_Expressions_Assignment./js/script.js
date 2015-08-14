760cd8b9e233921d3a1391e28ac59b48efc188e5
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-13 20:46:05 -0400

    First Commit

--- Ramos_Jesus_Expressions_Assignment./js/script.js
+++ Ramos_Jesus_Expressions_Assignment./js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+var carlSalary = 3500;// Carl's Salaray per month
+var  sueSalary= 2500;//Sue's Salary per month
+var salary = carlSalary + sueSalary;
+console.log(salary);
