48893ab369c9c840f83eb0aa706f101baef0ac63
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-13 22:49:33 -0400

    Variables Created

--- Ramos_Jesus_Expressions_Assignment./js/script.js
+++ Ramos_Jesus_Expressions_Assignment./js/script.js
@@ -1,4 +1,8 @@
-var carlSalary = 3500;// Carl's Salaray per month
-var  sueSalary= 2500;//Sue's Salary per month
-var salary = carlSalary + sueSalary;
-console.log(salary);
+
+
+//Variables 
+var myResidenceTown;
+var MyResidenceState;
+var yearsLivingInState;
+var usCitizen = true;
+
