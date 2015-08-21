693ce3d4958cadd551b91d79186ccba728d0109b
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-20 21:19:42 -0400

    Third Commit

--- Ramos_Jesus_Conditionals_Assignment/js/script.js
+++ Ramos_Jesus_Conditionals_Assignment/js/script.js
@@ -2,7 +2,7 @@
 
 //Variables
 var myName;//Age of the person wanting to go to the movies
-var currentYear = 2015;//current year used to make age calculation
+//var currentYear = 2015;//current year used to make age calculation
 var rMovieAge = 18;//Age to watch a Movie rated R
 var gMovieAge = 12;// Age to watch a Movie rated G
 var pg13MovieAge = 13;//Age to watch a movie rated PG13
@@ -12,13 +12,13 @@ var peopleInParty;// Age to watch a movie with parents
 
 // prompts
 myName = prompt("Welcome to Full sail Movies, please enter you name");
-userAge = prompt("In order to watch a movie enter your year of birth ");
+userAge = prompt("In order to watch a movie enter your age ");
 //peopleInParty = prompt("How many people in party?");
 
 
 userAge = Number(userAge);// casts the input as number
 
 //outputs
-console.log("thank you"+" " + myName + " " + "for choosing Full sail Cinema, your are" + userAge - currentYear + "years old");
-//console.log();
+console.log("thank you"+" " + myName + " " + "for choosing Full sail Cinema");
+console.log("your are" + userAge + "years old");
 //console.log();
