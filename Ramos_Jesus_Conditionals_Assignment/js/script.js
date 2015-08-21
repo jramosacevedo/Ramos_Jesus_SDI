7c2972e50a897c5a05ccb88c82011cbe273ea0ba
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-20 21:06:16 -0400

    Second Commit

--- Ramos_Jesus_Conditionals_Assignment/js/script.js
+++ Ramos_Jesus_Conditionals_Assignment/js/script.js
@@ -1,20 +1,24 @@
 // // Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Conditionals Assignment
 
 //Variables
-var myResidenceCity;//City of residence
-var MyResidenceState;//State of residence
-var yearsLivingInState;//Years in state
-var usCitizen = true;
+var myName;//Age of the person wanting to go to the movies
+var currentYear = 2015;//current year used to make age calculation
+var rMovieAge = 18;//Age to watch a Movie rated R
+var gMovieAge = 12;// Age to watch a Movie rated G
+var pg13MovieAge = 13;//Age to watch a movie rated PG13
+var userAge; // Age of user calculated using myAge variable
+var peopleInParty;// Age to watch a movie with parents
+
 
 // prompts
+myName = prompt("Welcome to Full sail Movies, please enter you name");
+userAge = prompt("In order to watch a movie enter your year of birth ");
+//peopleInParty = prompt("How many people in party?");
 
-myResidenceCity = prompt("Enter your City");
-myResidenceState = prompt("Enter your State");
-yearsLivingInstate = prompt("Years living there");
 
-yearsLivingInState = Number(yearsLivingInstate);// casts the input as number
+userAge = Number(userAge);// casts the input as number
 
 //outputs
-console.log("You live in " + myResidenceCity + " " + myResidenceState);// State and city of residence output
-console.log("it is" + " " + usCitizen +" " + "that you are a US Citizen");
-console.log("You have been living there for " + yearsLivingInState - 5 + " years more than me");//time that you lived there
+console.log("thank you"+" " + myName + " " + "for choosing Full sail Cinema, your are" + userAge - currentYear + "years old");
+//console.log();
+//console.log();
