37bbac4301c194ea6dbc0b1e080f6b1a930039db
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-20 21:30:31 -0400

    fourth Commit

--- Ramos_Jesus_Conditionals_Assignment/js/script.js
+++ Ramos_Jesus_Conditionals_Assignment/js/script.js
@@ -2,23 +2,28 @@
 
 //Variables
 var myName;//Age of the person wanting to go to the movies
-//var currentYear = 2015;//current year used to make age calculation
+var currentYear = 2015;//current year used to make age calculation
 var rMovieAge = 18;//Age to watch a Movie rated R
 var gMovieAge = 12;// Age to watch a Movie rated G
 var pg13MovieAge = 13;//Age to watch a movie rated PG13
 var userAge; // Age of user calculated using myAge variable
 var peopleInParty;// Age to watch a movie with parents
+var rMovies = 4
+var gMovies = 5
+var pG13Movies = 10
 
 
 // prompts
 myName = prompt("Welcome to Full sail Movies, please enter you name");
-userAge = prompt("In order to watch a movie enter your age ");
-//peopleInParty = prompt("How many people in party?");
+userAge = prompt("In order to watch a movie enter the year you were born ");
+peopleInParty = prompt("How many people in party?");
 
+//Casts
+peopleInParty = Number(peopleInParty);// casts the input as number
+userAge = Number(userAge);
 
-userAge = Number(userAge);// casts the input as number
 
 //outputs
 console.log("thank you"+" " + myName + " " + "for choosing Full sail Cinema");
-console.log("your are" + userAge + "years old");
-//console.log();
+console.log("your are" + (2015 - userAge) + "years old");
+console.log("You have" + " " + (peopleInParty -1) + "in your party");
