a983e1501ee99e92740d0b707608c7fc3268c8e1
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-25 14:46:33 -0400

    Third Commit

--- Ramos_Jesus_Functions_Assignment./js/script.js
+++ Ramos_Jesus_Functions_Assignment./js/script.js
@@ -1,11 +1,14 @@
 // // Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Functions Assignment
+//Variables
+var myRandom
+
 
 function randomNumGen() {
 
     var randomNum = [];
 
-    for (var i = 0; i < 4; i++) {
-        randomNum[i] = Math.random() * (20 - 10);
+    for (var i = 0; i < 6; i++) {
+        randomNum[i] = Math.random() * (53 - 1);
         randomNum[i] = Math.round(randomNum[i]);
 
     }
