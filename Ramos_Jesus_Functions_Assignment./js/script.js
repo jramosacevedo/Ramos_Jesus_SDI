d87caef1054f249b52a936eb8afa7fa2db3a9043
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-26 13:33:56 -0400

    Created script for powerball

--- Ramos_Jesus_Functions_Assignment./js/script.js
+++ Ramos_Jesus_Functions_Assignment./js/script.js
@@ -1,20 +1,36 @@
-// // Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Functions Assignment
-//Variables
+// Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Functions Assignment
+
 var lottoNumbers
+var powerBallNumber
 
-
+//Lotto Generator
 function randomNumGen(max, min) {
 
-    var randomNum = [];
+    var randomNum = [];//creates an array with random numbers
 
     for (var i = 0; i < 6; i++) {
-        randomNum[i] = Math.random() * (max - min +1) + min ;
-        randomNum[i] = Math.round(randomNum[i]);
+        randomNum[i] = Math.random() * (max - min) + min; // subtracts the max number from minimum number and adds the minumum
+        randomNum[i] = Math.round(randomNum[i]);// rounds the number to the next integer.
+
+    }
+    return randomNum;
+
+}
+//PowerBall Generator
+function powerBall(max, min) {
+
+    var randomNum = [];//creates an array with random numbers
+
+    for (var i = 0; i < 1; i++) {
+        randomNum[i] = Math.random() * (max - min) + min; // subtracts the max number from minimum number and adds the minumum
+        randomNum[i] = Math.round(randomNum[i]);// rounds the number to the next integer.
 
     }
     return randomNum;
 
 }
 
-lottoNumbers = randomNumGen(1, 53);
+powerBallNumber = powerBall (1, 52);
+lottoNumbers = randomNumGen(1, 52);
 console.log(" Here are your numbers." + lottoNumbers);
+console.log("Here is your Powerball" + powerBallNumber);
