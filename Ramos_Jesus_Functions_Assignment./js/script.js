96dc6fe98ee1628476d136d56009a06a46251f9b
Jesus Ramos <jramosacevedo@fullsail.edu>
2015-08-29 11:59:52 -0400

    New code for prompt

--- Ramos_Jesus_Functions_Assignment./js/script.js
+++ Ramos_Jesus_Functions_Assignment./js/script.js
@@ -1,7 +1,28 @@
 // Jesus Ramos Acevedo // Scalable Data Infrastructure // Term 02// Functions Assignment
 
-var lottoNumbers
-var powerBallNumber
+
+loop:            //label statement used with break/continue
+    while(true){
+        var game = prompt("What lottery do you play? (FL/PowerBall )"); //prompt user to enter either FL/Powerball
+        if(game !==null)               //check to see the game is null or not i.e user clicks cancel button.
+        {
+            game.toLowerCase();        //toLowerCase method for case insensitive code
+        }
+        switch (game) {             //switch stmt for checking whether FL/Powerball
+            case "fl":
+                field = "Florida_lottery"; //main/global variable for FL
+                n=53;                      //main/global variable for FL
+                break loop;
+            case "powerball":
+                field = "Powerball_lottery";   // main/global variable for powerball
+                n=59;                          // main/global variable for powerball
+                break loop;
+
+            default: alert("Please enter either lottery type/Spell Check");  //default message if user click incorrect lottery name/clicks cancel
+        }
+    }
+
+
 
 //Lotto Generator
 function randomNumGen(max, min) {
@@ -16,12 +37,13 @@ function randomNumGen(max, min) {
     return randomNum;
 
 }
+
 //PowerBall Generator
 function powerBall(max, min) {
 
     var randomNum = [];//creates an array with random numbers
 
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < 6; i++) {
         randomNum[i] = Math.random() * (max - min) + min; // subtracts the max number from minimum number and adds the minumum
         randomNum[i] = Math.round(randomNum[i]);// rounds the number to the next integer.
 
@@ -32,5 +54,5 @@ function powerBall(max, min) {
 
 powerBallNumber = powerBall (1, 52);
 lottoNumbers = randomNumGen(1, 52);
-console.log(" Here are your numbers." + lottoNumbers);
+console.log(" " +lottoNumbers + " ");
 console.log("Here is your Powerball" + powerBallNumber);
